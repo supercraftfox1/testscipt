523527f21435c17b5dce47f53f443a3dafc523a5
supercraftfox1 <74929027+supercraftfox1@users.noreply.github.com>
2020-12-06 17:44:17 +0300

    Update start.bash

--- start.bash
+++ start.bash
@@ -3,8 +3,14 @@ apt install openssh-server nano wget sudo
 sudo sh -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
 wget https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb
 sudo dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
+sleep 10
 hamachi start
-hamachi set-nick servertesting12342153
+sleep 10
+hamachi set-nick servertesting12342153343
+sleep 10
 hamachi login
-hamachi create server-testing-12342153
+sleep 10
+hamachi create server-testing-12342153f4
+sleep 10
 hamachi go-online
+sleep 10
