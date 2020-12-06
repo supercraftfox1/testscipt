3fe25662956bb100de164c90d1ae99535c47a862
supercraftfox1 <74929027+supercraftfox1@users.noreply.github.com>
2020-12-06 18:25:41 +0300

    Update start.bash

--- start.bash
+++ start.bash
@@ -7,11 +7,11 @@ sudo dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
 sleep 5
 hamachi start
 sleep 5
-hamachi set-nick servertesting12342153343fd24r
+hamachi set-nick servertesting12342153343fd24rr
 sleep 5
 hamachi login
 sleep 5
-hamachi create server-testing-12342153f4fd4g2 1232
+hamachi create server-testing-12342153f4fd4g2v 1232
 sleep 5
 hamachi go-online
 sleep 5
